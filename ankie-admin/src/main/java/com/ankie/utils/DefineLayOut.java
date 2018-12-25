ebdf46f3d3d796bf0e1fae825bf5d117ff4c0c7e
ankiebee <biweifeiwinner@163.com>
2018-12-25 17:09:34 +0800

    启动报错优化

---
+++ ankie-admin/src/main/java/com/ankie/utils/DefineLayOut.java
@@ -0,0 +1,13 @@
+package com.ankie.utils;
+
+import org.apache.log4j.HTMLLayout;
+
+public class DefineLayOut extends HTMLLayout {
+	
+	public String getContentType() {
+		// ÐÞ¸Ä±àÂë·½Ê½
+		return "text/html;charset=UTF-8"; 
+	}
+
+}
+
