700366886ffdb8f51d9eb7ba8a95625158a138e5
ankiebee <biweifeiwinner@163.com>
2018-12-27 10:43:40 +0800

    页面访问异常修复

--- ankie-admin/src/main/java/com/ankie/controller/HomeController.java
+++ ankie-admin/src/main/java/com/ankie/controller/HomeController.java
@@ -3,18 +3,11 @@ package com.ankie.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
-/**
- * 
- * @Description
- * @author biweifei
- * @time 2018年12月26日 下午5:01:25
- */
 @Controller
 public class HomeController {
-	
-	@RequestMapping("/home")
-	public String home() {
-		return "test";
-	}
+
+    @RequestMapping("/hello")
+    public String hello(){        
+        return "home";
+    }
 }
